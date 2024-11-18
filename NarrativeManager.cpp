cdecee59f0ed9afac79196c6e5cc436f4510e203
Mantequilla Electromagnética <A01708423@tec.mx>
2024-11-17 21:52:35 -0600

    Update NarrativeManager.cpp

--- NarrativeManager.cpp
+++ NarrativeManager.cpp
@@ -1,11 +1,42 @@
 #include "NarrativeManager.h"
 #include <string>
+#include <iostream>
+#include <fstream>
+#include <vector>
 using namespace std;
-
-void NarrativeManager::readStoryScript(int _round){
-
+// Constructor
+NarrativeManager :: NarrativeManager(): Manager(){
+    textStoryName = "QuietDreadStory.txt";
+    storyLine = "";
+    storyScriptSize = 0;
 }
-
-string NarrativeManager:: getStoryLine(){
-    
+// Reads the .txt file and returns the desired text line (string).
+string NarrativeManager:: getStoryLine(int _round){
+    vector <string> contents;
+    string text;
+    ifstream file(textStoryName);
+    if(file.fail()){
+        cerr << "Error" << endl;
+        return "";
+    } 
+    while(getline(file, text)){
+            contents.push_back(text);
+    }
+    file.close();
+    if (_round < 0 || _round >= contents.size()){
+        cerr << "Error" << endl;
+        return "";
+    }
+    storyScriptSize = contents.size();
+    return contents[_round];
+}
+// Returns the ending text line according to the type of ending of _character.
+string NarrativeManager:: getEndingScene(string _character){
+    if (_character == "Demon"){
+        return getStoryLine(storyScriptSize - 3);
+    } else if (_character == "Player"){
+        return getStoryLine(storyScriptSize - 2);
+    } else {
+        return getStoryLine(storyScriptSize - 1);
+    }
 }
