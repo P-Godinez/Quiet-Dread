a66b2bb415513ef5fc8f023952dee733617c35af
Mantequilla Electromagnética <A01708423@tec.mx>
2024-11-17 21:50:12 -0600

    Update ChoiceManager.cpp

--- ChoiceManager.cpp
+++ ChoiceManager.cpp
@@ -1,18 +1,29 @@
 # include "ChoiceManager.h"
-
-int ChoiceManager::getChoice(){
-
+// Constructor.
+ChoiceManager:: ChoiceManager():Manager(){
+    insanityEffects = 0;
+    impatienceEffects = 0;
+    totalNumberOfChoices = 4;
 }
-
 int ChoiceManager::getImpatienceEffects(){
-
+    return impatienceEffects;
 }
-
 int ChoiceManager::getInsanityEffects(){
-
+    return insanityEffects;
 }
-
-void evaluateChoice(int _choice){
-    
+// Will modify the insanity and impatience effects based on the choice made as well as taking into account the current round number.
+void ChoiceManager:: evaluateChoice(int _choice){
+    if (_choice == 1){
+        insanityEffects = 20 * (roundNumber/totalNumberOfChoices);
+        impatienceEffects = 0;
+    } else if (_choice == 2){
+        insanityEffects = 0;
+        impatienceEffects = 20 * (roundNumber/totalNumberOfChoices);
+    } else {
+        insanityEffects = 20 * (roundNumber/totalNumberOfChoices);
+        impatienceEffects = 20 * (roundNumber/totalNumberOfChoices);
+    }
 }
-
+int ChoiceManager:: getTotalChoices(){
+    return totalNumberOfChoices;
+}
