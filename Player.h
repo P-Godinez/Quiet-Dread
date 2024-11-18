85f2692060331cd4237f2cc9d210cb23d5176b6b
Mantequilla Electromagnética <A01708423@tec.mx>
2024-11-17 21:53:25 -0600

    Update Player.h

--- Player.h
+++ Player.h
@@ -1,7 +1,7 @@
-class Player{
-    private: 
-        int insanity;
+# pragma once
+# include "Character.h"
+class Player: public Character{
     public: 
+        void setInsanity(int);
         int getInsanity();
-        void changeInsanity(int stat);
 };
