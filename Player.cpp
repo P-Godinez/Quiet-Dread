4d2fd12d240d100f685a672d0c7da5ace0f10963
Mantequilla Electromagnética <A01708423@tec.mx>
2024-11-17 21:53:09 -0600

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -1,10 +1,8 @@
 #include "Player.h"
-
-void Player::changeInsanity(int _stat){
-
+// Despite being called a setter, it will increase the stat, not change it by the _stat parameter, but rather sum it with the _stat parameter.
+void Player::setInsanity(int _stat){
+    stat += _stat;
 }
-
 int Player:: getInsanity(){
-    
+    return stat;
 }
-
