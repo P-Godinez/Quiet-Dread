346c47c595a11f431a78dde1e3d5a0c862f6fca3
Mantequilla Electromagnética <A01708423@tec.mx>
2024-11-17 21:50:44 -0600

    Update Demon.cpp

--- Demon.cpp
+++ Demon.cpp
@@ -1,9 +1,9 @@
 #include "Demon.h"
-
-void Demon:: changeImpatience(int _stat){
-
+// Despite being called a setter, it will increase the stat, not change it by the _stat parameter, but rather sum it with the _stat parameter.
+void Demon:: setImpatience(int _stat){
+    stat += _stat;
 }
 
 int Demon:: getImpatience(){
-    
+    return stat;
 }
