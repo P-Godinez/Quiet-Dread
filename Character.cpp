238dec912a6daa0da4112e4cd0f73aa8f0cbf5c9
Mantequilla Electromagnética <A01708423@tec.mx>
2024-11-17 21:49:30 -0600

    Update Character.cpp

--- Character.cpp
+++ Character.cpp
@@ -1,9 +1,13 @@
-#include "Character.h"
-// 
-bool Character::checkStatus(){
-
+# include "Character.h"
+// Default constructor
+Character:: Character(){
+    stat = 0;
 }
-
-void Character:: reset(){
-    
+// Checks is the character is still active.
+bool Character:: checkStatus(){
+    if (stat >= 100){
+        return false;
+    } else {
+        return true;
+    }
 }
