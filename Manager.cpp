d7a83c62293eecbeeb3808eb687bf37df5b71831
Mantequilla Electromagnética <A01708423@tec.mx>
2024-11-22 20:27:26 -0600

    Update Manager.cpp

--- Manager.cpp
+++ Manager.cpp
@@ -1,13 +1,11 @@
 # include "Manager.h"
+Manager:: Manager(){
+    roundNumber = 0;
+} 
 // increments the round number (line in the .txt file). 
 void Manager :: incrementRound(int _num){
     roundNumber = roundNumber + _num;
 }
-// constructor.
-Manager:: Manager(){
-    roundNumber = 0;
-} 
-// returns the round number.
 int Manager:: getRoundNumber(){
     return roundNumber;
 }
