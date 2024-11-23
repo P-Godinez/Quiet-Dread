e23cc26c667babd787e660f4fbce81e01581c942
Mantequilla Electromagnética <A01708423@tec.mx>
2024-11-22 20:29:54 -0600

    Update QuietDreadMain.cpp

--- QuietDreadMain.cpp
+++ QuietDreadMain.cpp
@@ -15,12 +15,12 @@ int main(){
     // START GAME with instructions and introduction.
     cout << "\n\n============== QUIET DREAD =============\n\n" << endl;
     g1.startGame();
-    cout << g1.getCurrentStoryLine() << endl;
+    cout << "\n " <<  g1.getCurrentStoryLine() << endl;
     g1.update();
     // Start choice cycle
     while(g1.checkIsPlaying() == true){
-         cout << g1.getCurrentStoryLine() << endl;
-         cout << "Enter choice number: ";
+         cout << "\n " << g1.getCurrentStoryLine() << endl;
+         cout << "\n " << "Enter choice number: ";
          cin >> currentChoice;
          while(currentChoice <= 0 || currentChoice > 3){
             cout << "Enter 1, 2 or 3."<< endl;
@@ -29,13 +29,13 @@ int main(){
          g1.instantUpdateRound(currentChoice - 1);
          g1.setStats(currentChoice);
          g1.update();
-         cout << g1.getCurrentStoryLine() << endl;
+         cout << "\n " << g1.getCurrentStoryLine() << endl;
          cout << g1.getGameStats() << endl;
          g1.reverseUpdateRound(currentChoice - 1);
     }
     // Get ending scene based on the user's choices. 
-    cout << g1.getCurrentEnding() << endl;
-    cout << "Press any number to exit game." << endl;
+    cout << "\n " << g1.getCurrentEnding() << endl;
+    cout << "\n " << "Thank you for playing \nPress any number to exit game." << endl;
     cin >> currentChoice;
     return 0;    
 }
